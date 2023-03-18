ec2510e1bb537f471c2ce2be5694300614d84ec4
VinothWso2 <123294208+VinothWso2@users.noreply.github.com>
2023-03-18 10:08:32 +0530

    Add download URL function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,24 +5,57 @@ import io.ballerinalang.compiler.text.TextDocument;
 import io.ballerinalang.compiler.text.TextDocuments;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Files;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class Main {
     public static void main(String[] args) {
-        Path dirPath = Paths.get("/home/vinoth/Documents/nballerina-main");
+            try {
+                String baseUrl = "https://github.com/ballerina-platform/ballerina-distribution/archive/refs/heads/master.zip";
+                Path savePath = Paths.get("/home/vinoth/Music");
+                String pathString = savePath.toString();
+        
+                // Download and extract the zip file
+                URL url = new URL(baseUrl);
+                ZipInputStream zipStream = new ZipInputStream(url.openStream());
+                ZipEntry entry = zipStream.getNextEntry();
+                while (entry != null) {
+                    Path filePath = Paths.get(pathString, entry.getName());
+                    if (!entry.isDirectory()) {
+                        // Create directories if necessary
+                        Files.createDirectories(filePath.getParent());
+                        // Write the file to disk
+                        FileOutputStream outputStream = new FileOutputStream(filePath.toFile());
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = zipStream.read(buffer)) != -1) {
+                            outputStream.write(buffer, 0, bytesRead);
+                        }
+                        outputStream.close();
+                    }
+                    zipStream.closeEntry();
+                    entry = zipStream.getNextEntry();
+                }
+                zipStream.close();
 
-        if (dirPath == null || !Files.isDirectory(dirPath)) {
-            System.out.println("Directory path is empty or invalid. Please provide a valid path");
-            return;
-        }
+            // continue with the rest of the code
+            Path dirPath = Paths.get(pathString);
 
-        try {
+            if (dirPath == null || !Files.isDirectory(dirPath)) {
+                System.out.println("Directory path is empty or invalid. Please provide a valid path");
+                return;
+            }
+
             Files.walk(dirPath)
                     .filter(path -> path.toString().endsWith(".bal"))
                     .forEach(filePath -> {
@@ -34,13 +67,15 @@ public class Main {
                             SyntaxTree syntaxTree = SyntaxTree.from(textDocument);
                             StatementVisitor visitor = new StatementVisitor();
                             syntaxTree.rootNode().accept(visitor);
-                            visitor.writeToCSV("output.csv");
+                            visitor.writeToCSV();
                         } catch (IOException e) {
                             System.out.println("Error reading file " + filePath + ": " + e.getMessage());
                         }
                     });
+        } catch (MalformedURLException e) {
+            System.out.println("Invalid URL: " + e.getMessage());
         } catch (IOException e) {
-            System.out.println("Error walking directory " + dirPath + ": " + e.getMessage());
+            System.out.println("Error downloading file: " + e.getMessage());
         }
     }
 
@@ -50,6 +85,10 @@ public class Main {
 
         private boolean isInsideLocalVar;
         private boolean isListBindingPattern;
+        private static final String OUTPUT_CSV_FILE = "output.csv";
+
+        // private static final Pattern BASIC_LITERAL_PATTERN =
+        //         Pattern.compile("(\\b(nil|true|false|[0-9]+|0x[0-9a-fA-F]+|\".*\"|\\[.*\\]|\\{.*\\}|\\(.*\\))\\b)");
 
         @Override
         public void visit(CaptureBindingPatternNode node) {
@@ -62,22 +101,38 @@ public class Main {
                 return;
             }
 
+            // Check if the right-hand side of the parent statement is a basic literal value
+            StatementNode parentStatement = getParentStatement(node.variableName());
+            // if (parentStatement != null && parentStatement instanceof ExpressionStatementNode) {
+            //     ExpressionNode expressionNode = ((ExpressionStatementNode) parentStatement).expression();
+            //     if (expressionNode instanceof BasicLiteralNode) {
+            //         return;
+            //     }
+            // }
+
             Token variableToken = node.variableName();
             String variableName = variableToken.toSourceCode();
 
             variableName = variableName.replaceAll("\\s+", " "); // remove unnecessary spaces
+
             variableNames.add(variableName.trim());
             System.out.println(variableName.trim());
-
-            StatementNode parentStatement = getParentStatement(variableToken);
 
             if (parentStatement != null) {
                 String sourceStatement = parentStatement.toSourceCode();
                 sourceStatement = sourceStatement.replaceAll("\\s+", " "); // remove unnecessary spaces
+
+                // skip the processing of the node if the parent statement contains a basic literal value
+                // if (BASIC_LITERAL_PATTERN.matcher(sourceStatement).find()) {
+                //     return;
+                // }
+
                 sourceStatements.add(sourceStatement.trim());
                 System.out.println(sourceStatement.trim());
+
             }
         }
+
 
         @Override
         public void visit(VariableDeclarationNode variableDeclarationNode) {
@@ -107,13 +162,13 @@ public class Main {
             return (StatementNode) parent;
         }
 
-        private void writeToCSV(String fileName) {
+        private void writeToCSV() {
             try {
 
-                FileWriter writer = new FileWriter(new File(fileName),true);
+                FileWriter writer = new FileWriter(new File(OUTPUT_CSV_FILE),true);
 
                 // read existing entries from the output.csv file into a set
-                Set<String> existingEntries = Files.lines(Paths.get(fileName))
+                Set<String> existingEntries = Files.lines(Paths.get(OUTPUT_CSV_FILE))
                         .map(String::trim)
                         .collect(Collectors.toSet());
 
@@ -123,15 +178,16 @@ public class Main {
                     }
                     String variableLabel = variableNames.get(i).trim();
                     String statementSourceCode = sourceStatements.get(i).trim();
-                    String combinedEntry = variableLabel + ",\"" + statementSourceCode + "\"";
+                    String combinedEntry = variableLabel + "," + statementSourceCode ;
 
-                    // check if the combined entry already exists in the set of existing entries
-                    if (!existingEntries.contains(combinedEntry)) {
-                        // write the combined entry to the output.csv file
-                        writer.write(combinedEntry + "\n");
-                        // add the combined entry to the set of existing entries
-                        existingEntries.add(combinedEntry);
-                    }
+
+                        // check if the combined entry already exists in the set of existing entries
+                        if (!existingEntries.contains(combinedEntry)) {
+                            // write the combined entry to the output.csv file
+                            writer.write(combinedEntry + "\n");
+                            // add the combined entry to the set of existing entries
+                            existingEntries.add(combinedEntry);
+                        }
                 }
 
                 writer.flush();
